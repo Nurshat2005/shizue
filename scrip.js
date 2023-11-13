478823ffd13b03af03ecdabdf11f7287d813e747
Nurshat2005 <kenjebekjusupov01@gmail.com>
2023-11-13 17:56:57 +0600

    [

---
+++ scrip.js
@@ -0,0 +1,12 @@
+const container =document.getElementById
+('container');
+const registerBtn=document.
+getElementById('register');
+const loginBtn =document.getElementById
+('login');
+
+registerBtn.addEventListener('click',()
+{container.classlist.add("active"); });
+
+loginBtnBtn.addEventListener('click',()
+{container.classlist.remove("active"); });
